d2d9c20a52fb946bf91071e688740046b490ac2a
Kevin Li <likevin2010@gmail.com>
2018-03-03 13:19:22 -0800

    add fetchNumberOfExercisesForType

--- frontend/src/provider/API.js
+++ frontend/src/provider/API.js
@@ -52,6 +52,45 @@ class API {
 
     return this._backend.update('/users/', userId, data);
   }
+
+  createExercise(name, imageURL, type) {
+    const data = {
+      Name: name,
+      ImageURL: imageURL,
+      Type: type
+    };
+
+    return this._backend.create('/exercises/', data);
+  }
+
+  addExerciseForUser(userId, exerciseId) {
+    const data = {
+      Exercises: {}
+    };
+    data.Exercises[exerciseId] = true;
+
+    return this._backend.update('/users/', userId, data);
+  }
+
+  async fetchNumberOfExercisesForType(userId, type) {
+    const user = await this.readUser(userId);
+
+    return this._backend.readAll('/exercises/')
+      .then(exercises => Object.keys(exercises).map(e =>
+        user.Exercises && e in user.Exercises && exercises[e].Type == type
+      ))
+      .then(result => result.filter(r => r).length);
+  }
+
+  async fetchNumberOfCompletedInSessionExercisesForType(userId, type) {
+    const user = await this.readUser(userId);
+
+    return this._backend.readAll('/exercises/')
+      .then(exercises => Object.keys(exercises).map(e =>
+        user.CurrentWorkoutExercises && e in user.CurrentWorkoutExercises && exercises[e].Type == type
+      ))
+      .then(result => result.filter(r => r).length);
+  }
 }
 
 module.exports = API;
