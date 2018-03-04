769dfdac294eca3dff31a8f8f92e2a3985608a64
Prithvi Narasimhan <narasimhan.prithvi@gmail.com>
2018-03-04 10:25:57 -0800

    BACKBUTTON

--- frontend/src/HeaderBar.js
+++ frontend/src/HeaderBar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './HeaderBar.css';
 import {MdArrowBack} from 'react-icons/lib/md';
-
+import { Router} from 'react-router-dom';
 
 class HeaderBar extends Component {
   constructor(props){
@@ -21,14 +21,21 @@ class HeaderBar extends Component {
   componentWillUnmount() {
     clearInterval(this.interval);
   }
+  goBack() {
+    window.history.back();
+  }
   render() {
     const workoutTime = `${this.calculateTimeDeltaInMinutes()} Minutes since workout started`
+    const styles = {};
+    if (this.props.hideWorkoutTime) {
+      styles['top'] = "10px";
+    }
     return (
       <div className="header_div">
-        <div>
-
+        <div className="back-div" onClick={this.goBack.bind(this)} style={styles}>
+          <img className="back-button" src="https://png.icons8.com/android/96/ffffff/circled-left.png" />
         </div>
-        <div>
+        <div class="content-header-div">
           <div className="workout_header">
             {this.props.header_title}
           </div>
