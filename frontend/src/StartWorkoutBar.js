063eadacf3589f93526373425f992f62692c671b
andyznew <akzhang@ucsd.edu>
2018-03-03 20:34:55 -0800

    added link from start page and added onClick

--- frontend/src/StartWorkoutBar.js
+++ frontend/src/StartWorkoutBar.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom'
 import './StartWorkoutBar.css';
 
 
 
 class StartWorkoutBar extends Component {
+  onLinkClick() {
+  	console.log('Clicked!')
+  }
   render() {
     return (
+    <Link to = '/Home' onClick={this.onLinkClick}>
       <div className="start_workout_div">
         Start Workout
       </div>
+     </Link>
     );
   }
 }
