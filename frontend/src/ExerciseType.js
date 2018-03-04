4a6bbc32629e0f9c5524cc0b2b9b4ece654c3d5d
Prithvi Narasimhan <narasimhan.prithvi@gmail.com>
2018-03-04 10:31:37 -0800

    FIX

--- frontend/src/ExerciseType.js
+++ frontend/src/ExerciseType.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import './ExerciseType.css';
+import {fetchRelativeURL} from './utils/FetchUserID';
+
 
 class ExerciseType extends Component {
   render() {
     const styles = {'backgroundColor': this.props.backgroundColor}
     return (
-      <Link to={`/group/${this.props.type}`} className="exercise_type_div hvr-pulse-shrink" style={styles}>
+      <Link to={fetchRelativeURL(`/group/${this.props.type}`)} className="exercise_type_div hvr-pulse-shrink" style={styles}>
         <img alt={this.props.imageName} src={this.props.imageName} className="exercise_image"/>
         <div className="exercise_details">
           {this.props.completed}/{this.props.total} {this.props.type}
