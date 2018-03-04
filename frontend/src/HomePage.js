6fec9a7d597cb8a664d197a28f201a094118cb0a
Kevin Li <likevin2010@gmail.com>
2018-03-03 23:11:35 -0800

    call fetchNumberOfExercisesForType in HomePage.js

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -8,32 +8,32 @@ import { api } from './provider/API';
 class HomePage extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      workoutData: {}
+    };
 
     api.getTimeOfWorkoutStart('-L6hunulp8Xw93HEn5DN')
       .then(startTime => this.setState({startTime}));
+
+    const workoutData = {};
+
+    for (const exerciseType of exerciseTypesList.data) {
+      workoutData[exerciseType.name] = {};
+
+      api.fetchNumberOfExercisesForType('-L6hunulp8Xw93HEn5DN', exerciseType.name)
+        .then(total => workoutData[exerciseType.name].total = total)
+        .then(() => api.fetchNumberOfCompletedInSessionExercisesForType('-L6hunulp8Xw93HEn5DN', exerciseType.name))
+        .then(completed => workoutData[exerciseType.name].completed = completed)
+        .then(() => this.setState({workoutData}));
+    }
   }
 
   render() {
-    const workoutData = {
-      Chest: {
-        completed: 3,
-        total: 6
-      },
-      Biceps:  {
-        completed: 2,
-        total: 5
-      },
-      Triceps:  {
-        completed: 2,
-        total: 10
-      }
-    }
     return (
       <div style={{"textAlign": "center"}}>
         <HeaderBar header_title="
           Workout for 5/5/18" timeStart={this.state.startTime} />
-        <ExerciseTypes workoutData={workoutData}/>
+        <ExerciseTypes workoutData={this.state.workoutData}/>
         <CompleteWorkout />
       </div>
     );
