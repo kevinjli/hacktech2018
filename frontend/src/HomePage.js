89368b27acba544ff99df72aae3b40c8e7b33344
Kevin Li <likevin2010@gmail.com>
2018-03-03 20:41:06 -0800

    call getTimeOfWorkoutStart in HomePage.js

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import HeaderBar from './HeaderBar';
 import ExerciseTypes from './ExerciseTypes';
 import CompleteWorkout from './CompleteWorkout';
+import { api } from './provider/API';
 
 class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    api.getTimeOfWorkoutStart('-L6hunulp8Xw93HEn5DN')
+      .then(startTime => this.setState({startTime}));
+  }
+
   render() {
     return (
       <div style={{"textAlign": "center"}}>
         <HeaderBar header_title="
-          Workout for 5/5/18" timeStart="0" />
+          Workout for 5/5/18" timeStart={this.state.startTime} />
         <ExerciseTypes />
         <CompleteWorkout />
       </div>
